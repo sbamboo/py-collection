f557f6abfd82076a99ec9fd81e7172f41754e982
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-06-09 23:51:51 +0200

    start work on stringTags 1.2

---
+++ scripts/liblike/stringtags/1.2/stringTags.py
@@ -0,0 +1,15 @@
+# StringTags: Smal library to handle some nice string formatting.
+# Made by: Simon Kalmi Claesson
+# Version: 1.2
+#
+
+# [Imports]
+import socket
+import re
+import getpass
+
+# CustomTags {<tagname>} PARSED FIRST AND GETS PRIO OVER BUILT-INT TAGS,
+#                        CAN ONLY CONTAIN [A-Z][a-z][9-0]_-
+# 
+
+# Main function
