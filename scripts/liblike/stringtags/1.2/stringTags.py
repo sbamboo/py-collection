6b211186169f8e9aa6eb14dd01e96f12d5c45e70
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-06-10 22:06:32 +0200

    Update stringTags.py

--- scripts/liblike/stringtags/1.2/stringTags.py
+++ scripts/liblike/stringtags/1.2/stringTags.py
@@ -8,8 +8,32 @@ import socket
 import re
 import getpass
 
-# CustomTags {<tagname>} PARSED FIRST AND GETS PRIO OVER BUILT-INT TAGS,
-#                        CAN ONLY CONTAIN [A-Z][a-z][9-0]_-
-# 
+#                                     TAGNAMEs CAN ONLY CONTAIN `[A-Z][a-z][9-0]_-`
+#
+# Custom_Tags      {<tagName>}        PARSED FIRST AND GETS PRIO, SO THEY CAN CONTAIN TAGS.
+#
+# BuiltIn_Tags     {<tagName>}        PARSED SECONDLY.
+#
+# Placeholders     {<sym>} / §<sym>§  ONLY BUILTIN SYMBOLS:
+#                                    "{esc}"=>"\033"  /  "§esc§"=>"\033"
+#                                    "{\n}"=>"\n"     /  "§nl§"=>"\n"
+#
+# Unicode          {u.<unicode>}      ONLY UNICODE-CODEPOINTS IN CURRENT ENCODING
+#
+# Variable/Method  [<var/method>]     ONLY OBJS IN "allowedVariables" CAN USE SUBOBJS USING `.`
+#                                    CALLS "Callables" AND RETURNS VALUE FOR THE REST.
+#
+# PaletteKeys      {p:<paletteKey>}   RETRIVES FORMATTING FROM "Palette"
+#
+# AnsiCode         {!<AnsiCode>}      FOR COLOR DON'T FORGET m AT THE END
+#
+# Hex Foreground   {#<hex6d>}         NO OPACITY SUPPORT
+# Hex Background   {#!<hex6d>}        NO OPACITY SUPPORT
+#
+# RGB Foreground   {rgb.<R>;<G>;<B>}  INTEGER ONLY
+# RGB Background   {rgb!<R>;<G>;<B>}  INTEGER ONLY
+#
+# Unformatted_Tags {^<tagName>}       WON'T BE FORMATTED SINCE LAST
+#
 
 # Main function
